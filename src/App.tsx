8c4e6519930963e13dbd651df5dc1297a1904285
frankgonzales22 <137746118+frankgonzales22@users.noreply.github.com>
2023-07-07 15:34:58 +0800

    Configure React app for deployment to GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Message } from "./Components/Message"
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <div>Something</div>
+      <Message />
     </>
   )
 }

db1d68610c463bda7b40e5debb3c0129a1fcec76
frankgonzales22 <137746118+frankgonzales22@users.noreply.github.com>
2023-07-07 18:07:48 +0800

    Configure React app for deployment to GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react"
 import { AddItem } from "./Components/AddItem"
 import { Display } from "./Components/Display"
 import './index.css'
 
 function App() {
+const [item, setItem] = useState("")
+const [title, setTitle] = useState("")
 
+// const print = () => {
+//   console.log(e.target.value)
+// }
 
   return (
     <>
+    {<h1>TITLE : </h1>} {JSON.stringify(title)}
+    <br />
+    {<h2>ITEM : </h2>} {JSON.stringify(item)}
       <div className="container">
-        <AddItem />
+        <AddItem 
+        onChangeInput={(e:React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)} 
+        onChangeTextArea={(e:React.ChangeEvent<HTMLTextAreaElement>) => setItem(e.target.value)}/>
         <Display />
       </div>
     </>
--- src/Components/AddItem.tsx
+++ src/Components/AddItem.tsx
@@ -1,20 +1,44 @@
 
 
-export const AddItem = () => {
+interface AddItemProps {
+    onChangeInput : (e:React.ChangeEvent<HTMLInputElement>) => void
+    onChangeTextArea : (e:React.ChangeEvent<HTMLTextAreaElement>) => void
+    onClickButton : React.MouseEvent<HTMLButtonElement> 
+}
+
+
+export const AddItem = ({ onChangeInput, onChangeTextArea, onClickButton }: AddItemProps) => {
+  
+    
     return (
         <>
+       
             <div className="container d.flex">
                 <div className="mb-3">
                     <label htmlFor="exampleFormControlInput1" className="form-label">Todo Title</label>
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="My Title" />
+                    <input type="email" 
+                    className="form-control" 
+                    id="exampleFormControlInput1" 
+                    placeholder="My Title" 
+                    onChange={onChangeInput}
+                    />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleFormControlTextarea1" className="form-label">Description</label>
-                    <textarea className="form-control textArea" id="exampleFormControlTextarea1" placeholder="Doing laundry..."></textarea>
-                    
+                    <textarea 
+                    className="form-control textArea" 
+                    id="exampleFormControlTextarea1" 
+                    placeholder="Doing laundry..." 
+                    onChange={onChangeTextArea}>
+                  
+                    </textarea>
+
                 </div>
-                <button className="btn btn-info">Add</button>
+                <button 
+                className="btn btn-info"
+                
+                >Add</button>
             </div>
-        </> 
-    )
+        </>
+    ) 
 }
